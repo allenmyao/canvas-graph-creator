634a62f09fdfe0791a9a8997f8646e6e038e5078
Allen Yao <alleyao@gmail.com>
2016-01-28 07:24:52 +0000

    Add node and edge creation

--- src/app.js
+++ src/app.js
@@ -1,9 +1,95 @@
 import 'babel-polyfill';
 
+import {Graph} from './graph';
 import {Node} from './node';
 import {Edge} from './edge';
 
+var canvas;
+var context;
+var graph;
+var canvasLeft;
+var canvasTop;
 
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+function init() {
 
+    canvas = document.getElementById('canvas');
+    canvas.setAttribute('width', 800);
+    canvas.setAttribute('height', 600);
+
+    canvasLeft = canvas.offsetLeft;
+    canvasTop = canvas.offsetTop;
+
+    context = canvas.getContext('2d');
+    // context.imageSmoothingEnabled = true;
+
+    graph = new Graph();
+
+    canvas.addEventListener('click', clickEventListener, false);
+
+    window.requestAnimationFrame(draw);
+}
+
+function draw() {
+    graph.draw(context);
+    window.requestAnimationFrame(draw);
+}
+
+window.addEventListener('load', init, false);
+
+function clickEventListener(event) {
+    let x = event.pageX - canvasLeft;
+    let y = event.pageY - canvasTop;
+
+    let selectedNode = null;
+    let startNode = null;
+    graph.forEachNode((node) => {
+        // console.log('checking node ' + node.id);
+
+        if (node.containsPoint(x, y)) {
+            if (node.isSelected) {
+                node.isSelected = false;
+            } else if (!selectedNode) {
+                node.isSelected = true;
+                selectedNode = node;
+            }
+        } else if (node.isSelected) {
+            startNode = node;
+        }
+
+        if (selectedNode && startNode) {
+            return false;
+        }
+    });
+
+    if (selectedNode && startNode) {
+        // check if edge already exists
+        if (graph.hasEdge(startNode, selectedNode)) {
+            console.log('Edge already exists');
+
+            selectedNode.isSelected = false;
+            // startNode.isSelected = false;
+            return;
+        }
+
+        let edge = new Edge(startNode, selectedNode);
+        graph.addEdge(edge);
+
+        startNode.isSelected = false;
+        selectedNode.isSelected = false;
+    } else if (!selectedNode) {
+        let isTooClose = false;
+        graph.forEachNode((node) => {
+            if (node.distanceToPoint(x, y) < 2 * Node.radius + 10) {
+                console.log('Too close to node ' + node.id);
+                isTooClose = true;
+                return false;
+            }
+        });
+
+        if (!isTooClose) {
+            let node = new Node(x, y);
+            graph.addNode(node);
+        }
+    }
+
+}
