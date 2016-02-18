227d7bed1938d72f9b0f1d6be1e4d1923882db40
Austin Kramer <ajkrame2@illinois.edu>
2016-02-17 20:46:16 -0600

    Refactored Mouse Listeners and added Action Cancellation on Mouse Drift

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ import {Edge} from './edge';
 var canvas;
 var context;
 var graph;
-var stateFunction;//Holds the function that will be applied when we click on stuff
+
+var tempTarget;     // Initial mouse-press target. Tools may care if the mouse remains on it upon unpress.
+var mouseDown;      // Is the mouse being held?
+var stateFuncDown;  // The function that will be applied when we click
+var stateFuncUp;    // The function that will be applied when we unclick
+var stateFuncMove;  // The function that will be applied when we move the mouse
  
 function init() {
  
@@ -19,14 +24,32 @@ function init() {
     // context.imageSmoothingEnabled = true;
  
     graph = new Graph();
+
+    tempTarget = null;
+    mouseDown = false;
+    stateFuncDown = function(x,y) {tempTarget = mouseSelect(x,y);};
+    stateFuncUp = function(x,y) {nodeClick(x,y); tempTarget = null;};
+    stateFuncMove = function(x,y) {};
  
-    stateFunction = function(x,y) {return nodeClick(x,y);};
- 
-    canvas.addEventListener('click', clickEventListener, false);
- 
+    canvas.addEventListener('mousedown', downEventListener, false);
+    canvas.addEventListener('mouseup', upEventListener, false);
+    canvas.addEventListener('mousemove', moveEventListener, false);
+
+
     window.requestAnimationFrame(draw);
 }
- 
+
+function mouseSelect(x,y) {
+  let selectedNode = null;
+  graph.forEachNode((node) => {
+    if (node.containsPoint(x, y)) {
+      selectedNode = node;
+      return false;
+    }
+  });
+  return selectedNode;
+}
+
 function nodeClick(x,y) {//split up more functionality to the move listener
   let selectedNode = null;
   let startNode = null;
@@ -34,10 +57,7 @@ function nodeClick(x,y) {//split up more functionality to the move listener
       // console.log('checking node ' + node.id);
  
       if (node.containsPoint(x, y)) {
-          if (node.isSelected) {
-              node.isSelected = false;
-          } else if (!selectedNode) {
-              node.isSelected = true;
+          if (node.isSelected || !selectedNode) {
               selectedNode = node;
           }
       } else if (node.isSelected) {
@@ -48,6 +68,21 @@ function nodeClick(x,y) {//split up more functionality to the move listener
           return false;
       }
   });
+
+  // If the mouse has drifted to a different object, cancel.
+  if (selectedNode != tempTarget) {
+    console.log('Mouse Drift');
+    return;
+  }
+
+  // Are we selecting or deselecting?
+  if (selectedNode) {
+    if (!selectedNode.isSelected) {
+      selectedNode.isSelected = true;
+    } else {
+      selectedNode.isSelected = false;
+    }
+  }
  
   if (selectedNode && startNode) {
       // check if edge already exists
@@ -88,14 +123,36 @@ function draw() {
 }
  
 window.addEventListener('load', init, false);
- 
-function clickEventListener(event) {
+
+function downEventListener(event) {
+    mouseDown = true;
+    let canvasLeft = canvas.offsetLeft;
+    let canvasTop = canvas.offsetTop;
+
+    let x = event.pageX - canvasLeft;
+    let y = event.pageY - canvasTop;
+
+    stateFuncDown(x,y);
+}
+
+function upEventListener(event) {
+    mouseDown = false;
+    let canvasLeft = canvas.offsetLeft;
+    let canvasTop = canvas.offsetTop;
+
+    let x = event.pageX - canvasLeft;
+    let y = event.pageY - canvasTop;
+
+    stateFuncUp(x,y);
+}
+
+function moveEventListener(event) {
     let canvasLeft = canvas.offsetLeft;
     let canvasTop = canvas.offsetTop;
  
     let x = event.pageX - canvasLeft;
     let y = event.pageY - canvasTop;
  
-    stateFunction(x,y);
+    stateFuncMove(x,y);
 }
 
