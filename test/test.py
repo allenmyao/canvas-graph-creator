a64f546756b7aac320c1947400e2523451ba1991
Nicholas Lu <ndlu2@illinois.edu>
2016-02-19 16:27:16 -0600

    refactored code and introduced helper methods/objects

--- test/test.py
+++ test/test.py
@@ -4,27 +4,67 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 
-def main(driver, browser):
-	driver.get("http://localhost:8080/index.html")
-	canvas = driver.find_element_by_xpath('//*[@id="canvas"]')
+def main(robot):
 
-	test(driver, canvas, browser)
+	robot.loadSite('http://localhost:8080/index.html', '//*[@id="canvas"]')
 
-	driver.close()
+	test(robot)
+
+	data.close()
 	return
 
 
-def test(driver, canvas , browser):
-	ActionChains(driver).move_to_element_with_offset(canvas, 30, 30).click().perform()
-	driver.get_screenshot_as_file(browser + '_1.png')
+def test(robot):
+	node1 = robot.createNode(30, 30)
+	robot.screenshot('1')
 
-	ActionChains(driver).move_to_element_with_offset(canvas, 180, 180).click().perform()
-	driver.get_screenshot_as_file(browser + "_2.png")
+	node2 = robot.createNode(180, 180)
+	robot.screenshot('2')
 
-	ActionChains(driver).move_to_element_with_offset(canvas, 50, 50).click().perform()
-	driver.get_screenshot_as_file(browser + '_3.png')
+	node1.select()
+	node2.select(10, 10)
 	return
+
+
+class Node:
+	def __init__(self, parent, x, y):
+		self.parent = parent;
+		self.x = x
+		self.y = y
+
+		parent.click(x, y)
+
+
+	def select(self,  offset_x = 0, offset_y = 0):
+		self.parent.click(self.x + offset_x, self.y + offset_y)
+		
+
+class Robot:
+	def __init__(self, driver, name):
+		self.driver = driver
+		self.name = name
+		self.nodes = []
+
+	def click(self, x, y):
+		ActionChains(self.driver).move_to_element_with_offset(self.canvas, x , y).click().perform()
+
+	def loadSite(self, url, canvas_xpath):
+		self.driver.get(url);
+		self.canvas = self.driver.find_element_by_xpath(canvas_xpath);
+
+	def createNode(self, x, y):
+		node = Node(self, x, y)
+		self.nodes.append(node)
+		return node
+
+	def close(self):
+		self.driver.close()
+
+	def screenshot(self, name):
+		self.driver.get_screenshot_as_file(self.name + '_' + name + '.png')
+
 
 if __name__ == "__main__":
-    main(webdriver.Firefox(), "Firefox")
-    main(webdriver.Chrome(), "Chrome")
+	main(Robot(webdriver.Firefox(), "Firefox"))
+	main(Robot(webdriver.Chrome(), "Chrome"))
+
