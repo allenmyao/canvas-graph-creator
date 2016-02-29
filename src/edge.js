48de58919cf3df63d1d308e929ec468c30eabdc1
nkelly132 <kelly68@illinois.edu>
2016-02-28 20:28:49 -0800

    Refactoring edge class for efficiency and ease of use
    Started by redoing the constructor.  It now expects an additional parameter bezierPoint = {x:,y:}.  Naming of variables has been improved for readability, these changes have been propogated to the rest of the code base.  Member variables have been increased to accommodate more necessary information, like start and end point, and the type of cost (weight vs grammar) the edge is currently using.  Code has been included in the constructor to calculate the costType as well as the bezier point is not provided, as only two arguments are required:  the starting node and destination node.
    
    Optimized drawing, now the start and end points are calculated only when they change instead of every call to draw.  For now this is only in the constructor, need to ensure this occurs when nodes are moved as well.

--- src/edge.js
+++ src/edge.js
@@ -1,40 +1,66 @@
 export class Edge {
+  
+  constructor(startNode, destNode, bezierPoint = null, cost = null, isDirected = false) {
+    let methods = [
+      'draw'
+    ];
 
-    constructor(start, dest, weight = null, isDirected = false) {
-        this.start = start;
-        this.dest = dest;
-        this.weight = weight;
-        this.isDirected = isDirected;
+    for (let method of methods) {
+      if (this[method] === undefined || typeof this[method] !== 'function') {
+        throw TypeError('Must override method: ' + method);
+      }
     }
 
-    containsPoint(x, y) {
-        // TODO: implement this
-        return false;
+    if (arguments.length < 2) {
+      throw Error(`Edge constructor requires at least two arguments: startNode and destNode. Actually passed in ${arguments}`);
     }
-
-    draw(context) {
-        let startPoint;
-        let destPoint;
-        try {
-            startPoint = this.start.edgePointInDirection(this.dest.x, this.dest.y);
-            destPoint = this.dest.edgePointInDirection(this.start.x, this.start.y);
-        } catch (e) {
-            return;
-        }
-
-        context.strokeStyle = 'black';
-
-        // Create a new path
-        context.beginPath();
-
-        // Start path at given point
-        context.moveTo(startPoint.x, startPoint.y);
-
-        // Draw line to given point
-        context.lineTo(destPoint.x, destPoint.y);
-
-        // Draw to the canvas
-        context.stroke();
+    this.costType = null;
+    this.startNode = startNode;
+    this.destNode = destNode;
+    this.bezierPoint = bezierPoint;
+    this.cost = cost;
+    this.isDirected = isDirected;
+    
+    try {
+      this.startPoint = this.startNode.edgePointInDirection(this.destNode.x, this.destNode.y);
+      this.destPoint = this.destNode.edgePointInDirection(this.startNode.x, this.startNode.y);
+    } catch (e) {
+      return;
     }
+  
+    if(bezierPoint === null) {
+      bezierPoint = {
+          x: (this.startPoint.x + this.destPoint.x)/2,
+          y: (this.startPoint.y + this.destPoint.y)/2
+        };
+    }
+    
+    if(typeof cost === 'string' || cost instanceof String) {
+      costType = false;
+    } else if(typeof cost === 'number') {
+      costType = true;
+    }
+  }
+
+  containsPoint(x, y) {
+    // TODO: implement this
+    return false;
+  }
+  
+  draw(context) {
+    context.strokeStyle = 'black';
+
+    // Create a new path
+    context.beginPath();
+
+    // Start path at given point
+    context.moveTo(this.startPoint.x, this.startPoint.y);
+
+    // Draw line to given point
+    context.lineTo(this.destPoint.x, this.destPoint.y);
+
+    // Draw to the canvas
+    context.stroke();
+  }
 
 }
