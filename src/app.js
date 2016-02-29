fc6dc0387760e6ed2fed688d73e72ad6fda1b582
Allen Yao <alleyao@gmail.com>
2016-02-29 07:21:25 +0000

    Add zoom to mouse position on scroll

--- src/app.js
+++ src/app.js
@@ -18,6 +18,13 @@ var toolMap = {
     move: new MoveTool()
 };
 
+const SCALE_MODIFIER = 0.9;
+
+// scale of 0.5 means drawing at half size
+let scale = 1;
+let dx = 0;
+let dy = 0;
+
 function init() {
     canvas = document.getElementById('canvas');
     context = canvas.getContext('2d');
@@ -31,37 +38,68 @@ function init() {
     window.requestAnimationFrame(draw);
 }
 
+function getCanvasX(event) {
+        let canvasX = event.offsetX;
+        let x = canvasX / scale + dx;
+        return x;
+}
+
+function getCanvasY(event) {
+        let canvasY = event.offsetY;
+        let y = canvasY / scale + dy;
+        return y;
+}
+
 function initMouseHandler() {
     mouseHandler = new MouseHandler(graph);
 
     canvas.addEventListener('mousedown', (event) => {
-        let canvasLeft = canvas.offsetLeft;
-        let canvasTop = canvas.offsetTop;
-
-        let x = event.pageX - canvasLeft;
-        let y = event.pageY - canvasTop;
+        let x = getCanvasX(event);
+        let y = getCanvasY(event);
 
         mouseHandler.downListener(event, currentTool, x, y);
     }, false);
 
     canvas.addEventListener('mouseup', (event) => {
-        let canvasLeft = canvas.offsetLeft;
-        let canvasTop = canvas.offsetTop;
-
-        let x = event.pageX - canvasLeft;
-        let y = event.pageY - canvasTop;
+        let x = getCanvasX(event);
+        let y = getCanvasY(event);
 
         mouseHandler.upListener(event, currentTool, x, y);
     }, false);
 
     canvas.addEventListener('mousemove', (event) => {
-        let canvasLeft = canvas.offsetLeft;
-        let canvasTop = canvas.offsetTop;
-
-        let x = event.pageX - canvasLeft;
-        let y = event.pageY - canvasTop;
+        let x = getCanvasX(event);
+        let y = getCanvasY(event);
 
         mouseHandler.moveListener(event, currentTool, x, y);
+    }, false);
+
+    canvas.addEventListener('wheel', (event) => {
+        // console.log(`mouse: (${event.offsetX}, ${event.offsetY})`);
+        // console.log(`origin: (${dx}, ${dy})`);
+        // prevent page scrolling (the default scroll behavior)
+        event.preventDefault();
+
+        let oldScale = scale;
+
+        let delta = event.deltaY;
+        if (delta > 0) {
+            // scroll down
+            scale = Math.max(scale * SCALE_MODIFIER, 0.1);
+        } else if (delta < 0) {
+            // scroll up
+            scale = Math.min(scale / SCALE_MODIFIER, 10);
+        }
+
+        let mouseX = event.offsetX;
+        let mouseY = event.offsetY;
+        dx += mouseX / oldScale - mouseX / scale;
+        dy += mouseY / oldScale - mouseY / scale;
+
+        context.resetTransform();
+        // context.setTransform(); does the translate first then scale
+        context.scale(scale, scale);
+        context.translate(-dx, -dy);
     }, false);
 }
 
@@ -121,7 +159,7 @@ function showModes() {
 }
 
 function draw() {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.clearRect(dx, dy, canvas.width / scale, canvas.height / scale);
     graph.draw(context);
     window.requestAnimationFrame(draw);
 }
