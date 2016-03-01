0f6593f722e91e9966b4fcae23f3c22e3e873dd8
Nicholas Lu <ndlu2@illinois.edu>
2016-03-01 17:53:40 -0600

    changed jenkins script to be able to start and stop

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,23 @@
-#!/bin/sh
-fuser -k 8080/tcp
-npm install
-gulp &
-cd selenium-tests
-mvn test
-cd ..
-fuser -k 8080/tcp
+#!/bin/bash
+
+function stop {
+	fuser -k 8080/tcp > /dev/null
+}
+
+function start {
+	stop
+	npm install > /dev/null
+	gulp &
+}
+
+if [[ "$1" = "start" ]]; then
+	start
+elif [[ "$1" = "stop" ]]; then
+	stop
+else
+	start
+	cd selenium-tests
+	mvn test
+	cd ..
+	stop
+fi
