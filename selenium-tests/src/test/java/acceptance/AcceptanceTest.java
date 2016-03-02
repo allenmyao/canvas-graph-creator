1da83a671caeac3ea36d2c8862aa125279845052
Nicholas Lu <ndlu2@illinois.edu>
2016-03-02 00:20:57 -0600

    making cucumber compile correctly

--- selenium-tests/src/test/java/acceptance/AcceptanceTest.java
+++ selenium-tests/src/test/java/acceptance/AcceptanceTest.java
@@ -1,8 +1,11 @@
 package acceptance;
+
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
+@CucumberOptions(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class AcceptanceTest {
 }
+
