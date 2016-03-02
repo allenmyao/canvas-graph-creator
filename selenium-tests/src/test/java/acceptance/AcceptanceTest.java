d47e04f5cb7b37532d2362df59805d139d7e4131
Nicholas Lu <ndlu2@illinois.edu>
2016-03-01 23:58:35 -0600

    Modified test to output json

--- selenium-tests/src/test/java/acceptance/AcceptanceTest.java
+++ selenium-tests/src/test/java/acceptance/AcceptanceTest.java
@@ -3,5 +3,6 @@ import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
+@Cucumber.Options(format = {"pretty", "html:target/cucumber", "json:target/cucumber.json"})
 public class AcceptanceTest {
 }
