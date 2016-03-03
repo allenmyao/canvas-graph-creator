3563b95cfce22342b10911f96c76f2d395c3ca15
Allen Yao <alleyao@gmail.com>
2016-03-03 22:47:27 +0000

    Add comments explaining zoom calculation

--- src/ui/canvas.js
+++ src/ui/canvas.js
@@ -7,7 +7,7 @@ let context;
 let mouseHandler;
 
 const SCALE_MODIFIER = 0.9;
-// scale of 0.5 means drawing at half size
+// scale of 0.5 means drawing at half size (0.5 pixels for each original pixel)
 let scale = 1;
 let dx = 0;
 let dy = 0;
@@ -67,8 +67,10 @@ function initMouseHandler(graph) {
         // prevent page scrolling (the default scroll behavior)
         event.preventDefault();
 
+        // store the current scale value
         let oldScale = scale;
 
+        // get the amount the mousewheel was scrolled
         let delta = event.deltaY;
         if (delta > 0) {
             // scroll down
@@ -78,14 +80,53 @@ function initMouseHandler(graph) {
             scale = Math.min(scale / SCALE_MODIFIER, 10);
         }
 
+        // get the mouse position (relative to the canvas element)
         let mouseX = event.offsetX;
         let mouseY = event.offsetY;
+
+        // Calculate the new position of the displayed origin (top left corner).
+        // (dx,dy) are the coordinates of the top-left corner.
+        // (x1,y1) and (x2,y2) are the coordinates at the mouse position before
+        // and after changing the scale, respectively.
+        //
+        // With the old scale value:
+        //     dx1 = dx
+        //     dy1 = dy
+        //     x1 = dx + mouseX / oldScale
+        //     y1 = dy + mouseY / oldScale
+        //
+        // With new scale:
+        //     dx2 = dx + mouseX / oldScale - mouseX / scale
+        //     dy2 = dy + mouseY / oldScale - mouseY / scale
+        //     x2 = dx2 + mouseX / scale
+        //        = dx + mouseX / oldScale - mouseX / scale + mouseX / scale
+        //        = dx + mouseX / oldScale
+        //        = x1
+        //     y2 = dy2 + mouseY / scale
+        //        = dy + mouseY / oldScale - mouseY / scale + mouseY / scale
+        //        = dy + mouseY / oldScale
+        //        = y1
+        //
+        // Note that (x1,y1) = (x2,y2) which means the coordinates under
+        // the mouse stays the same.
         dx += mouseX / oldScale - mouseX / scale;
         dy += mouseY / oldScale - mouseY / scale;
 
+        // reset the transformations done to the canvas
         context.resetTransform();
-        // context.setTransform(); does the translate first then scale
+
+        // context.setTransform(xScale, xSkew, ySkew, yScale, dx, dy) applies
+        // the translation before the scale.
+        // This is not what we want, since the scale is done relative to (0,0)
+        // and the position displayed at the top-left corner will no longer
+        // be (dx,dy), which means the coordinates under the mouse will change.
+
+        // scale both x- and y-axis
         context.scale(scale, scale);
+
+        // context.translate(a, b) translates the canvas origin by (a,b).
+        // Since (dx,dy) are the coordinates calculated for the new origin, the
+        // canvas needs to be translated by (-dx,-dy).
         context.translate(-dx, -dy);
 
         UI.updateZoom(scale);
