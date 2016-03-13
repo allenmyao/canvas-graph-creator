674d3301d2e841851367bfa9344c81b9f618a7eb
Allen Yao <alleyao@gmail.com>
2016-03-13 16:04:42 +0000

    Add tabs to sidebar

--- src/js/ui/sidebar.js
+++ src/js/ui/sidebar.js
@@ -1,7 +1,16 @@
+import { Tabs } from 'ui/tabs';
+
 let sidebar;
+let tabs;
 
 export function init() {
   sidebar = document.getElementById('sidebar');
+
+  let tabContainer = sidebar.children[0];
+  let tabList = tabContainer.children[0];
+
+  tabs = new Tabs(tabContainer, tabList);
+  tabs.init();
 }
 
 export function getSidebar() {
