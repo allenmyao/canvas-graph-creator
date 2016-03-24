b2916c9548025e73f5f758e93d561262b0c00d59
nkelly132 <kelly68@illinois.edu>
2016-03-23 23:39:41 -0700

    more notes

--- src/js/util/algorithm.js
+++ src/js/util/algorithm.js
@@ -1,10 +1,12 @@
+//Notes:  how can I store graphical state, separate context?
+
 import defaultMember from "\src\js\util\queue.js";
 import "\src\js\util\graphLogic.js";
 import "\src\js\util\graphVisuals.js";
 
 export class Abstract() {  
   constructor(graph, input) {
-    this.queue = new Queue();
+    this.queue = new Queue();//use this queue class for state queue; will get long.  use native js style queues for short queues.  implement in class?
     this.graph = graph;
     this.input = input;
     this.curNode = null;
--- src/js/util/graphVisuals.js
+++ src/js/util/graphVisuals.js
@@ -1,1 +1,1 @@
-//TODO:  brainstorm visual effects of graphLogic operators, functionality in node and edge classes vs gathered in one spot, here, 
+//TODO:  brainstorm visual effects of graphLogic operators, functionality in node and edge classes vs gathered in one spot, here?
