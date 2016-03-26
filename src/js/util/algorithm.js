eb1218a7aa3f1180d562d114e35330a202b1b898
Allen Yao <alleyao@gmail.com>
2016-03-26 23:24:55 +0000

    Fix linting errors

--- src/js/util/algorithm.js
+++ src/js/util/algorithm.js
@@ -1,24 +1,20 @@
-//Notes:  how can I store graphical state, separate context?
+// Notes:  how can I store graphical state, separate context?
 
-import defaultMember from "\src\js\util\queue.js";
-import "\src\js\util\graphLogic.js";
-import "\src\js\util\graphVisuals.js";
+import Queue from '/src/js/util/queue.js';
+import '/src/js/util/graphLogic.js';
+import '/src/js/util/graphVisuals.js';
 
-export class Abstract() {  
+export class Abstract {
   constructor(graph, input) {
-    this.queue = new Queue();//use this queue class for state queue; will get long.  use native js style queues for short queues.  implement in class?
+    // use this queue class for state queue; will get long.  use native js style queues for short queues.  implement in class?
+    this.queue = new Queue();
     this.graph = graph;
     this.input = input;
     this.curNode = null;
   }
-  
-  nextStep() {
-    
-  }
-  
-  backStep() {
-    
-  }
-  
-  
+
+  nextStep() {}
+
+  backStep() {}
+
 }
--- src/js/util/stepper.js
+++ src/js/util/stepper.js
@@ -1,32 +1,19 @@
-//NOTES:  make this responsible for GUI elements as well, move to UI folder?  should draw knowing little to nothing about container, difficult to not make assumptions though
-//how does timekeeping work in javascript?
+// NOTES:  make this responsible for GUI elements as well, move to UI folder?  should draw knowing little to nothing about container, difficult to not make assumptions though
+// how does timekeeping work in javascript?
 
-export class Stepper() {
+export class Stepper {
   constructor() {
-    
+
   }
-  
-  stepBack() {
-    
-    
-  }
-  
-  stepForward() {
-    
-    
-  }
-  
-  pause() {
-    
-  }
-  
-  play() {
-    
-  }
-  
-  setSpeed() {
-    
-    
-  }
-  
+
+  stepBack() {}
+
+  stepForward() {}
+
+  pause() {}
+
+  play() {}
+
+  setSpeed(speed) {}
+
 }
