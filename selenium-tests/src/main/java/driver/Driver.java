e1e8ff55d82871c6f78b59d2abaa2c5b6d0e4e68
Nicholas Lu <ndlu2@illinois.edu>
2016-03-29 16:00:15 -0500

    ...

--- selenium-tests/src/main/java/driver/Driver.java
+++ selenium-tests/src/main/java/driver/Driver.java
@@ -36,7 +36,7 @@ public class Driver {
 	//#TODO load from file
 	public static final String USERNAME = System.getenv("SAUCE_USERNAME");
 	public static final String ACCESS_KEY = System.getenv("SAUCE_ACCESS_KEY");
-	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:4445/wd/hub";
+	public static final String URL = "http://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:80/wd/hub";
 	
 	protected WebDriver driver;
 	protected String name;
