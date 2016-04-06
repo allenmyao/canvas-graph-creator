090bb09d307881f1cb2bb331b532dc69325fbd94
Allen Yao <alleyao@gmail.com>
2016-04-06 09:30:39 +0000

    Fix SelectTool not deselecting object when switching tool

--- src/js/tool/select-tool.js
+++ src/js/tool/select-tool.js
@@ -1,6 +1,4 @@
 import { Tool } from '../tool/tool';
-// import * as UI from 'ui/ui';
-// import * as Sidebar from '../ui/sidebar';
 
 export class SelectTool extends Tool {
 
@@ -9,25 +7,25 @@ export class SelectTool extends Tool {
 
   selectedObject = null;
 
+  cancel() {
+    this.selectNone();
+  }
+
   selectObject(event, graph, obj, x, y) {
-    // UI.selectObject(obj);
     if (this.selectedObject) {
       this.selectedObject.isSelected = false;
     }
     obj.isSelected = true;
     this.selectedObject = obj;
-    // Sidebar.updateSidebar(obj);
   }
 
   // drag: multiselect?
 
   selectNone(event, graph, x, y) {
-    // UI.selectObject(graph);
     if (this.selectedObject) {
       this.selectedObject.isSelected = false;
       this.selectedObject = null;
     }
-    // Sidebar.updateSidebar(graph);
   }
 
 }
