ff6dad21839fd8d3173a9cfd74fcf42044d66e06
Allen Yao <alleyao@gmail.com>
2016-04-08 23:32:57 +0000

    Refactor edge classes and remove unused code

--- src/js/data/edge/dashed-edge.js
+++ src/js/data/edge/dashed-edge.js
@@ -4,7 +4,8 @@ import { drawArrows } from '../../util/curvedEdge';
 export class DashedEdge extends Edge {
 
   draw(context) {
-    context.strokeStyle = this.isSelected ? 'red' : this.color;
+    context.strokeStyle = this.isSelected ? this.selectedColor : this.color;
+    context.lineWidth = this.lineWidth;
     context.setLineDash([ 5, 5 ]);
 
     // Create a new path
@@ -21,20 +22,13 @@ export class DashedEdge extends Edge {
     context.setLineDash([]);
 
     if (this.isDirected) {
+      context.fillStyle = this.isSelected ? this.selectedColor : this.color;
       drawArrows(this, false, true);
     }
 
     if (this.edgeLabel !== '') {
-      context.font = '14px Arial';
-      context.fillStyle = 'black';
-      context.fillText(this.edgeLabel, this.xText, this.yText);
-      if (this.showTextCtrl) {
-        context.fillStyle = 'red';
-        context.beginPath();
-        context.arc(this.xText, this.yText, 3.0, 0, 1.5 * Math.PI);
-        context.lineTo(this.xText, this.yText);
-        context.fill();
-      }
+      this.drawLabel(context);
     }
   }
+
 }
