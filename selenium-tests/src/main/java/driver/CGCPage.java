be22af7cbd920941b7d7ded20dadc89fbd10b2f5
Allen Yao <alleyao@gmail.com>
2016-04-24 23:50:08 +0000

    Add selector for node tool

--- selenium-tests/src/main/java/driver/CGCPage.java
+++ selenium-tests/src/main/java/driver/CGCPage.java
@@ -15,27 +15,28 @@ public class CGCPage extends CanvasPage{
 
 	public static String CANVAS_CSS_SELECTOR = "#canvas";
 	public static String RESET_ZOOM_SELECTOR = "#reset-transform";
-	
+
 	public static String HOME_PAGE = "http://127.0.0.1:8080/";
 
 	public static String NODE_IMAGE = "src/test/resources/UnselectedNode.png";
 	public static BufferedImage EDGE_TOOL;
-	
+
 	public static Map<String, String> shortcuts;
 
 	static{
 	    try{
 	        //EDGE_TOOL = ImageIO.read(new File("src/test/resources/add_edge_tool.png"));
 	    	shortcuts = new HashMap<String, String>();
-	    	shortcuts.put("Edge", "#toolbar .tool[data-tool=\"edge\"]");
+				shortcuts.put("Edge", "#toolbar .tool[data-tool=\"edge\"]");
+	    	shortcuts.put("Node", "#toolbar .tool[data-tool=\"node\"]");
 	    	shortcuts.put("Toggle Directed Edge", "#context-menu > ul:nth-child(2) > li");
-	    	
+
 	    	shortcuts.put("Toggle Start State", "#context-menu > ul:nth-child(1) > li:nth-child(2)");
 	    	shortcuts.put("Toggle Accepting State", "#context-menu > ul:nth-child(1) > li:nth-child(1)");
-	    	
+
 	    	shortcuts.put("Delete Edge", "#context-menu > ul:nth-child(3) > li");
 	    	shortcuts.put("Delete Node", "#context-menu > ul:nth-child(3) > li");
-	    	
+
 	    	shortcuts.put("Add Circle Node", "#context-menu > ul.context-menu__section.context-menu__section--visible > li:nth-child(1)");
 	    }catch(final Exception ex){
 	        throw new RuntimeException("Failed to load resources", ex);
@@ -94,7 +95,7 @@ public class CGCPage extends CanvasPage{
 
 		selected = null;
 	}
-	
+
 	public void clickNode(Node node) {
 		clickCanvas(node.point);
 	}
@@ -102,7 +103,7 @@ public class CGCPage extends CanvasPage{
 	public void initialize(String website)
 	{
 		super.initialize(website);
-		
+
 		selectCanvas(CANVAS_CSS_SELECTOR);
 		//addElement(EDGE_TOOL, "edge tool");
 	}
