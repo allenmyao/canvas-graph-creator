e73be12908601c258c279fdc7e7cb2116f529cce
Austin Kramer <kramer.austin@sbcglobal.net>
2016-04-27 01:39:24 -0500

    Fixed a serializer test and documented Eraser Tool

--- src/js/tool/erase-tool.js
+++ src/js/tool/erase-tool.js
@@ -1,19 +1,48 @@
-import Tool from '../tool/tool';
-import Node from '../data/node/node';
-import Edge from '../data/edge/edge';
+import { Tool } from '../tool/tool';
+import { Node } from '../data/node/node';
+import { Edge } from '../data/edge/edge';
 
+/**
+ * Eraser Tool for removing nodes and edges from the graph using the mouse pointer.
+ */
 class EraseTool extends Tool {
 
   name = 'Erase Tool';
 
+   /**
+   * Commits to object selection when called by mouse handler
+   * @param {Object} event mouse event
+   * @param {Object} graph the current graph object
+   * @param {Object} obj the selectable object
+   * @param {number} x the mouse cursor's x position
+   * @param {number} y the mouse cursor's y position
+   * @returns {bool} true this tool needs selection data
+   */
   preSelectObject(event, graph, obj, x, y) {
     return true;
   }
 
+   /**
+   * Refuses to commit to object drag when called by mouse handler
+   * @param {Object} event mouse event
+   * @param {Object} graph the current graph object
+   * @param {Object} srcObj the selectable object
+   * @param {number} x the mouse cursor's x position
+   * @param {number} y the mouse cursor's y position
+   * @returns {bool} false this tool does not need drag data
+   */
   preDragObject(event, graph, srcObj, x, y) {
     return false;
   }
 
+   /**
+   * Deletes the selected object
+   * @param {Object} event mouse event
+   * @param {Object} graph the current graph object
+   * @param {Object} obj the object to delete
+   * @param {number} x the mouse cursor's x position
+   * @param {number} y the mouse cursor's y position
+   */
   selectObject(event, graph, obj, x, y) {
     if (obj instanceof Node) {
       graph.removeNode(obj);
@@ -22,6 +51,14 @@ class EraseTool extends Tool {
     }
   }
 
+   /**
+   * Deletes the dragged-over object
+   * @param {Object} event mouse event
+   * @param {Object} graph the current graph object
+   * @param {Object} obj the object to delete
+   * @param {number} x the mouse cursor's x position
+   * @param {number} y the mouse cursor's y position
+   */
   dragOverObject(event, graph, obj, x, y) {
     if (obj instanceof Node) {
       graph.removeNode(obj);
