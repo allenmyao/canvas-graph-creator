91788ce1077449c495d9fd4aa94588cae3c803b6
Allen Yao <alleyao@gmail.com>
2016-04-30 23:55:02 +0000

    Add documentation

--- src/js/tool/tool.js
+++ src/js/tool/tool.js
@@ -1,13 +1,22 @@
+/**
+ * Abstract superclass for all tool classes.
+ * @class Tool
+ */
 class Tool {
 
-  name; // displayed name
-  icon; // icon to display
   sidebarType = 'display';
 
-  constructor(icon) {
-    this.icon = icon;
+  /**
+   * Constructs a Tool instance. Should not be called directly.
+   * @constructs Tool
+   */
+  constructor() {
   }
 
+  /**
+   * Returns a boolean indicating whether or not the tool has modes.
+   * @return {boolean} - Whether or not the tool has modes.
+   */
   hasModes() {
     return typeof this.currentMode !== 'undefined'
         && typeof this.constructor.modes !== 'undefined'
@@ -15,11 +24,19 @@ class Tool {
         && typeof this.optionContent !== 'undefined';
   }
 
+  /**
+   * Returns a boolean indicating whether or not the tool has inputs.
+   * @return {boolean} - Whether or not the tool has inputs.
+   */
   hasInputs() {
     return typeof this.inputs !== 'undefined'
         && typeof this.inputTypes !== 'undefined';
   }
 
+  /**
+   * Returns a boolean indicating whether or not the tool has mode-specific inputs.
+   * @return {boolean} - Whether or not the tool has mode-specific inputs.
+   */
   hasModeInputs() {
     return typeof this.modeInputs !== 'undefined'
         && typeof this.modeInputTypes !== 'undefined';
@@ -29,22 +46,83 @@ class Tool {
     this.currentMode = mode;
   }
 
-  // These is called when changing tools
-  // Set up the interface
+  /**
+   * Called when changing to this tool (if necessary).
+   * @abstract
+   */
   activate() {}
-  // Undo any incomplete actions
+
+  /**
+   * Called when switching to a different tool to cancel any incomplete actions.
+   * @abstract
+   */
   cancel() {}
 
-  // mouse events on graph components
+  /**
+   * Returns a boolean indicating whether or not the tool should be able to handle selection.
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {(Node|Edge|Label)} obj - Selected graph component.
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   * @return {boolean} - Whether or not the tool should handle selection.
+   */
   preSelectObject(event, graph, obj, x, y) {
     return true;
   }
+
+  /**
+   * Returns a boolean indicating whether or not the tool should be able to handle dragging.
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {(Node|Edge|Label)} srcObj - Dragged graph component.
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   * @return {boolean} - Whether or not the tool should handle dragging.
+   */
   preDragObject(event, graph, srcObj, x, y) {
     return true;
   }
+
+  /**
+   * Handler for object selection.
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {(Node|Edge|Label)} obj - Selected graph component.
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   */
   selectObject(event, graph, obj, x, y) {}
+
+  /**
+   * Handler for object dragging.
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {(Node|Edge|Label)} srcObj - Dragged graph component.
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   */
   dragObject(event, graph, srcObj, startX, startY, x, y) {}
+
+  /**
+   * Handler for object dropping on another object.
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {(Node|Edge|Label)} droppedObj - Dropped graph component.
+   * @param  {(Node|Edge|Label)} destObj - Graph component that the object was dropped onto.
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   */
   dropOnObject(event, graph, droppedObj, destObj, startX, startY, x, y) {}
+
+  /**
+   * [dragOverObject description]
+   * @param  {Event} event - Event object from event listener.
+   * @param  {Graph} graph - The current graph object.
+   * @param  {[type]} obj    [description]
+   * @param  {number} x - Mouse x-coordinate (in canvas coordinates).
+   * @param  {number} y - Mouse y-coordinate (in canvas coordinates).
+   */
   dragOverObject(event, graph, obj, startX, startY, x, y) {
     return this.dragNone(event, graph, startX, startY, x, y);
   }
